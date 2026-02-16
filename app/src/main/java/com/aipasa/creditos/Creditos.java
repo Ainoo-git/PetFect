8534e685cd959d3859849b64962a033f2517f810
Ainoha <ainohayt06@gmail.com>
2026-02-16 17:49:14 +0100

    Creación sección para creditos

---
+++ app/src/main/java/com/aipasa/creditos/Creditos.java
@@ -0,0 +1,14 @@
+package com.aipasa.creditos;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.aipasa.R;
+
+public class Creditos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+    }
+}
