4714a82891f6488669ab1b93e932732445e7f2e4
Ainoha <ainohayt06@gmail.com>
2026-02-22 17:40:35 +0100

    Cambios en imagen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,7 @@ dependencies {
     // Implementaciones de google
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+
+    //Para el diseño de la card
+    implementation("com.google.android.material:material:1.11.0")
 }
